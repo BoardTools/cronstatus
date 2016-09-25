9b7a5e584b8a9342b80dd60526c46258c65390c3
Raphaël M <Galixte@users.noreply.github.com>
2016-09-25 05:56:09 +0200

    Update of FRENCH translation for Cron Status extension

--- language/fr/info_acp_cronstatus.php
+++ language/fr/info_acp_cronstatus.php
@@ -1,13 +1,17 @@
 <?php
 /**
 *
-* @package cronstatus
-* @copyright (c) 2014 John Peskens (http://ForumHulp.com) and Igor Lavrov (https://github.com/LavIgor)
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* Cron Status extension for the phpBB Forum Software package.
 * French translation by Galixte (http://www.galixte.com)
+*
+* @copyright (c) 2016 John Peskens (http://ForumHulp.com) and Igor Lavrov (https://github.com/LavIgor)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
 
+/**
+* DO NOT CHANGE
+*/
 if (!defined('IN_PHPBB'))
 {
 	exit;
@@ -18,7 +22,23 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
-	'ACP_CRON_STATUS_TITLE'				=> 'Statut  du Cron',
+	'ACP_CRON_STATUS_TITLE'				=> 'Statut du Cron',
 	'ACP_CRON_STATUS_CONFIG_TITLE'		=> 'Vérifier le statut du Cron',
 ));
