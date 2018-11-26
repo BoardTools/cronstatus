e41b698e6a6c84c88085d80cd2edafe748ab21c3
lavigor <lavigor@users.noreply.github.com>
2018-11-26 21:24:03 +0300

    Even more refactoring for Scrutinizer

--- acp/cronstatus_module.php
+++ acp/cronstatus_module.php
@@ -47,6 +47,20 @@ class cronstatus_module
 		{
 			case 'details':
 				$this->show_details($user, $request, $config, $phpbb_extension_manager, $template, $phpbb_root_path);
+
+				if ($request->is_ajax())
+				{
+					$template->assign_vars(array(
+						'IS_AJAX' => true,
+					));
+				}
+				else
+				{
+					$template->assign_vars(array(
+						'U_BACK' => $this->u_action,
+					));
+				}
+
 				$this->tpl_name = 'acp_ext_details';
 			break;
 
@@ -61,98 +75,8 @@ class cronstatus_module
 					meta_refresh(60, $this->u_action . '&amp;sk=' . $sk . '&amp;sd=' . $sd);
 				}
 
-				$task_array = array();
-				$tasks = $phpbb_container->get('cron.manager')->get_tasks();
+				$this->show_tasks($phpbb_container, $phpbb_dispatcher, $config, $user, $template, $request, $sk, $sd, $cron_type);
 
-				// Fall back on the previous method for phpBB <3.1.9
-				$cronlock = '';
-				$rows = $phpbb_container->get('boardtools.cronstatus.listener')->get_cron_tasks($cronlock);
-
-				if ($config['cronstatus_latest_task'])
-				{
-					$cronlock = $config['cronstatus_latest_task'];
-				}
-
-				if (sizeof($tasks) && is_array($rows))
-				{
-					/** @var \phpbb\cron\task\task $task */
-					foreach ($tasks as $task)
-					{
-						$task_name = $task->get_name();
-						if (empty($task_name))
-						{
-							continue;
-						}
-
-						$find = strpos($task_name, 'tidy');
-						if ($find !== false)
-						{
-							$name = substr($task_name, $find + 5);
-							$name = ($name == 'sessions') ? 'session' : $name;
-							$task_date = (int) $this->helper->array_find($name . '_last_gc', $rows);
-						}
-						else if (strpos($task_name, 'prune_notifications'))
-						{
-							$task_date = (int) $this->helper->array_find('read_notification_last_gc', $rows);
-							$name = 'read_notification';
-						}
-						else if (strpos($task_name, 'queue'))
-						{
-							$task_date = (int) $this->helper->array_find('last_queue_run', $rows);
-							$name = 'queue_interval';
-						}
-						else
-						{
-							$name = (strrpos($task_name, ".") !== false) ? substr($task_name, strrpos($task_name, ".") + 1) : $task_name;
-							$task_last_gc = $this->helper->array_find($name . '_last_gc', $rows);
-							$task_date = ($task_last_gc !== false) ? (int) $task_last_gc : -1;
-						}
-
-						$new_task_interval = ($task_date > 0) ? $this->helper->array_find($name . (($name != 'queue_interval') ? '_gc' : ''), $rows) : 0;
-						$new_task_date = ($new_task_interval > 0) ? $task_date + $new_task_interval : 0;
-
-						/**
-						 * Event to modify task variables before displaying cron information
-						 *
-						 * @event boardtools.cronstatus.modify_cron_task
-						 * @var object task          Task object
-						 * @var object task_name     Task name ($task->get_name())
-						 * @var object name          Task name for new task date
-						 * @var object task_date     Last task date
-						 * @var object new_task_date Next task date
-						 * @since 3.1.0-RC3
-						 * @changed 3.1.1 Added new_task_date variable
-						 */
-						$vars = array('task', 'task_name', 'name', 'task_date', 'new_task_date');
-						extract($phpbb_dispatcher->trigger_event('boardtools.cronstatus.modify_cron_task', compact($vars)));
-
-						$task_array[] = array(
-							'task_sort'       => ($task->is_ready()) ? 'ready' : 'not_ready',
-							'display_name'    => $task_name,
-							'task_date'       => $task_date,
-							'task_date_print' => ($task_date == -1) ? $user->lang['CRON_TASK_AUTO'] : (($task_date) ? $user->format_date($task_date, $config['cronstatus_dateformat']) : $user->lang['CRON_TASK_NEVER_STARTED']),
-							'new_date'        => $new_task_date,
-							'new_date_print'  => ($new_task_date > 0) ? $user->format_date($new_task_date, $config['cronstatus_dateformat']) : '-',
-							'task_ok'         => ($task_date > 0 && ($new_task_date > time())) ? false : true,
-							'locked'          => ($config['cron_lock'] && $cronlock == $name) ? true : false,
-						);
-					}
-					unset($tasks, $rows);
-
-					$task_array = $this->helper->array_sort($task_array, $sk, (($sd == 'a') ? SORT_ASC : SORT_DESC));
-
-					foreach ($task_array as $row)
-					{
-						$template->assign_block_vars($row['task_sort'], array(
-							'DISPLAY_NAME'  => $row['display_name'],
-							'TASK_DATE'     => $row['task_date_print'],
-							'NEW_DATE'      => $row['new_date_print'],
-							'TASK_OK'       => $row['task_ok'],
-							'LOCKED'        => $row['locked'],
-							'CRON_TASK_RUN' => ($request->is_ajax()) ? '' : (($row['display_name'] != $cron_type) ? '<a href="' . $this->u_action . '&amp;cron_type=' . $row['display_name'] . '&amp;sk=' . $sk . '&amp;sd=' . $sd . '" class="cron_run_link">' . $user->lang['CRON_TASK_RUN'] . '</a>' : '<span class="cron_running_update">' . $user->lang['CRON_TASK_RUNNING'] . '</span>'),
-						));
-					}
-				}
 				$template->assign_vars(array(
 					'U_ACTION'   => $this->u_action,
 					'U_NAME'     => $sk,
@@ -230,18 +154,114 @@ class cronstatus_module
 				'VERSIONCHECK_FAIL_REASON' => ($e->getMessage() !== $user->lang('VERSIONCHECK_FAIL')) ? $e->getMessage() : '',
 			));
 		}
+	}
 
-		if ($request->is_ajax())
+	/**
+	 * Assigns template parameters for available Cron tasks
+	 *
+	 * @param object                           $phpbb_container  phpBB container object
+	 * @param \phpbb\event\dispatcher          $phpbb_dispatcher phpBB dispatcher object
+	 * @param \phpbb\config\config             $config           Config object
+	 * @param \phpbb\user                      $user             User object
+	 * @param \phpbb\template\template         $template         Template object
+	 * @param \phpbb\request\request_interface $request          Request object
+	 * @param string                           $sk               Sort key
+	 * @param string                           $sd               Sort direction
+	 * @param string                           $cron_type        Name of Cron task for execution
+	 */
+	public function show_tasks($phpbb_container, \phpbb\event\dispatcher $phpbb_dispatcher, \phpbb\config\config $config, \phpbb\user $user, \phpbb\template\template $template, \phpbb\request\request_interface $request, $sk, $sd, $cron_type)
+	{
+		$task_array = array();
+		$tasks = $phpbb_container->get('cron.manager')->get_tasks();
+
+		// Fall back on the previous method for phpBB <3.1.9
+		$cronlock = '';
+		$rows = $phpbb_container->get('boardtools.cronstatus.listener')->get_cron_tasks($cronlock);
+
+		if ($config['cronstatus_latest_task'])
 		{
-			$template->assign_vars(array(
-				'IS_AJAX' => true,
-			));
+			$cronlock = $config['cronstatus_latest_task'];
 		}
-		else
+
+		if (sizeof($tasks) && is_array($rows))
 		{
-			$template->assign_vars(array(
-				'U_BACK' => $this->u_action,
-			));
+			/** @var \phpbb\cron\task\task $task */
+			foreach ($tasks as $task)
+			{
+				$task_name = $task->get_name();
+				if (empty($task_name))
+				{
+					continue;
+				}
+
+				$find = strpos($task_name, 'tidy');
+				if ($find !== false)
+				{
+					$name = substr($task_name, $find + 5);
+					$name = ($name == 'sessions') ? 'session' : $name;
+					$task_date = (int) $this->helper->array_find($name . '_last_gc', $rows);
+				}
+				else if (strpos($task_name, 'prune_notifications'))
+				{
+					$task_date = (int) $this->helper->array_find('read_notification_last_gc', $rows);
+					$name = 'read_notification';
+				}
+				else if (strpos($task_name, 'queue'))
+				{
+					$task_date = (int) $this->helper->array_find('last_queue_run', $rows);
+					$name = 'queue_interval';
+				}
+				else
+				{
+					$name = (strrpos($task_name, ".") !== false) ? substr($task_name, strrpos($task_name, ".") + 1) : $task_name;
+					$task_last_gc = $this->helper->array_find($name . '_last_gc', $rows);
+					$task_date = ($task_last_gc !== false) ? (int) $task_last_gc : -1;
+				}
+
+				$new_task_interval = ($task_date > 0) ? $this->helper->array_find($name . (($name != 'queue_interval') ? '_gc' : ''), $rows) : 0;
+				$new_task_date = ($new_task_interval > 0) ? $task_date + $new_task_interval : 0;
+
+				/**
+				 * Event to modify task variables before displaying cron information
+				 *
+				 * @event boardtools.cronstatus.modify_cron_task
+				 * @var object task          Task object
+				 * @var object task_name     Task name ($task->get_name())
+				 * @var object name          Task name for new task date
+				 * @var object task_date     Last task date
+				 * @var object new_task_date Next task date
+				 * @since 3.1.0-RC3
+				 * @changed 3.1.1 Added new_task_date variable
+				 */
+				$vars = array('task', 'task_name', 'name', 'task_date', 'new_task_date');
+				extract($phpbb_dispatcher->trigger_event('boardtools.cronstatus.modify_cron_task', compact($vars)));
+
+				$task_array[] = array(
+					'task_sort'       => ($task->is_ready()) ? 'ready' : 'not_ready',
+					'display_name'    => $task_name,
+					'task_date'       => $task_date,
+					'task_date_print' => ($task_date == -1) ? $user->lang['CRON_TASK_AUTO'] : (($task_date) ? $user->format_date($task_date, $config['cronstatus_dateformat']) : $user->lang['CRON_TASK_NEVER_STARTED']),
+					'new_date'        => $new_task_date,
+					'new_date_print'  => ($new_task_date > 0) ? $user->format_date($new_task_date, $config['cronstatus_dateformat']) : '-',
+					'task_ok'         => ($task_date > 0 && ($new_task_date > time())) ? false : true,
+					'locked'          => ($config['cron_lock'] && $cronlock == $name) ? true : false,
+				);
+			}
+			unset($tasks, $rows);
+
+			$task_array = $this->helper->array_sort($task_array, $sk, (($sd == 'a') ? SORT_ASC : SORT_DESC));
+
+			foreach ($task_array as $row)
+			{
+				$template->assign_block_vars($row['task_sort'], array(
+					'DISPLAY_NAME'  => $row['display_name'],
+					'TASK_DATE'     => $row['task_date_print'],
+					'NEW_DATE'      => $row['new_date_print'],
+					'TASK_OK'       => $row['task_ok'],
+					'LOCKED'        => $row['locked'],
+					'CRON_TASK_RUN' => ($request->is_ajax()) ? '' : (($row['display_name'] != $cron_type) ? '<a href="' . $this->u_action . '&amp;cron_type=' . $row['display_name'] . '&amp;sk=' . $sk . '&amp;sd=' . $sd . '" class="cron_run_link">' . $user->lang['CRON_TASK_RUN'] . '</a>' : '<span class="cron_running_update">' . $user->lang['CRON_TASK_RUNNING'] . '</span>'),
+				));
+			}
 		}
 	}
 }
